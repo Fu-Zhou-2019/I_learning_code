d748cd38cd6e921332698e89564f205ccdfbcd23
Fu-Zhou-2019 <2716559421@qq.com>
2020-11-25 21:16:03 +0800

    c_modern

---
+++ code_ex/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+void PrintN(int N);
+ 
+void PrintN(int N)
+{
+ 	int i;
+ 	for(i=1;i<N;i++)
+	 {
+		 printf("%d\n",i);
+	 }
+
+ } 
+ 
+
+
+
+int main()
+{
+	int N;	
+	printf("please enter number:");
+	scanf("%d", &N);
+	PrintN(N);
+	return 0;
+ }
+ 
+ 
+
